93075a46adfdee4b5ef0f55f333324ffd637dd53
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-04 19:33:00 +0530

    Profile page initial work done

--- client/components/ResponsiveSideBarOption.jsx
+++ client/components/ResponsiveSideBarOption.jsx
@@ -1,9 +1,12 @@
+import { redirect } from 'next/dist/server/api-utils'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 
 const ResponsiveSideBarOption = (props) => {
+  const router = useRouter()
   return (
-    <div className="w-min flex items-center rounded-[100px] p-4 cursor-pointer hover:bg-[#2e2d2d] transition-all hover:duration-200 hover:ease-in-out" onClick={() => { props.setSelected(props.text) }}>
+    <div className="w-min flex items-center rounded-[100px] p-4 cursor-pointer hover:bg-[#2e2d2d] transition-all hover:duration-200 hover:ease-in-out" onClick={() => { props.setSelected(props.text); router.push(props.redirect) }}>
 
 
       <div className="text-xl mr-4">
---
+++ client/pages/profile/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Sidebar from "../../components/SideBar"
+import RightSideBar from "../../components/RightSideBar"
+import ProfileHeader from './ProfileHeader'
+
+const Profile = () => {
+  return (
+    <div className='flex justify-center h-screen w-screen select-none bg-[#9ca3af] text-black'>
+      <div className='max-w-[1400px] w-2/3 flex justify-between'>
+
+        <Sidebar></Sidebar>
+        <div className='flex-[2] border-x border-r border-gray-500 overflow-y-scroll'>
+          <ProfileHeader></ProfileHeader>
+          <ProfileMessages></ProfileMessages>
+        </div>
+        <RightSideBar></RightSideBar>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;

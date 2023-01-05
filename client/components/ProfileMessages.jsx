8470093252d19f379d73c5a1b4408ddc5a973218
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-05 19:43:27 +0530

    Profile header added

--- client/components/ProfileMessages.jsx
+++ client/components/ProfileMessages.jsx
@@ -1,5 +1,57 @@
 import React from 'react'
 
+const DUMMY_DATA = [
+  {
+    displayName: "Nidhsih",
+    userName: "Nidhish07",
+    avatar: '',
+    text: 'Good morning',
+    isProfileImageNFT: false,
+    timestamp: '2020-7-17T12:00:00.000Z'
+
+
+  },
+  {
+    displayName: "Nidhsih",
+    userName: "Nidhish07",
+    avatar: '',
+    text: 'Good morning',
+    isProfileImageNFT: false,
+    timestamp: '2020-7-17T12:00:00.000Z'
+
+
+  },
+  {
+    displayName: "Nidhsih",
+    userName: "Nidhish07",
+    avatar: '',
+    text: 'Good morning',
+    isProfileImageNFT: false,
+    timestamp: '2020-7-17T12:00:00.000Z'
+
+
+  },
+  {
+    displayName: "Nidhsih",
+    userName: "Nidhish07",
+    avatar: '',
+    text: 'Good morning',
+    isProfileImageNFT: false,
+    timestamp: '2020-7-17T12:00:00.000Z'
+
+
+  },
+  {
+    displayName: "Nidhsih",
+    userName: "1FfmbHfnpaZjKFvyi1okTjJJusN455paPH",
+    avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg',
+    text: 'Good morning',
+    isProfileImageNFT: true,
+    timestamp: '2020-7-17T12:00:00.000Z'
+
+
+  },
+]
 const ProfileMessages = () => {
   return (
     <div>ProfileMessages</div>
